df9532d9f3663adf24203c3bbedd12210f2cd15b
Eduardo Marin <108038506+EduardoCamargoMarin@users.noreply.github.com>
2023-01-30 16:38:49 -0300

    Add files via upload

--- Filmes em cartaz/index.js
+++ Filmes em cartaz/index.js
@@ -1,18 +1,35 @@
 
-const frm = document.querySelector("form")
-const outputTitle = document.querySelector("h3")
-const outputTime = document.querySelector("h4")
+//const frm = document.querySelector("form")
+//const outputTitle = document.querySelector("h3")
+//const outputTime = document.querySelector("h4")
 
-frm.addEventListener("submit",function(e) {
-    const title = frm.inputTitulo.value
-    const duration = Number(fmr.duracao.value)
+// frm.addEventListener("submit", (e) => {
 
-    const hours = Math.floor(duration / 60)
-    const minutes = Math.floor(duration % 60)
+   // const title = String( frm.inputTitulo.value) 
+   // const duration = Number(fmr.duracao.value)
 
-    outputTitle.innerText = title
-    outputTime = `${hours} hours and ${minutes} minutes`
+  //  const hours = Math.floor(duration / 60)
+  //  const minutes = Math.floor(duration % 60)
 
-    e.preventDefault()
+ //   outputTitle.innerText = title
+  //  outputTime = `${hours} hours and ${minutes} minutes`
 
-})
+  //e.preventDefault()
+
+//})
+
+function calcular() {
+    const title = document.getElementById('inputTitulo').value
+    const duration = document.getElementById('duracao')
+
+    const outTitle = document.getElementById('outputTitle')
+    const outTime =  document.getElementById('outputTime')
+
+    const timeValue = Number(duration.value)
+
+    const hours = Math.floor(timeValue / 60)
+    const minutes = Math.floor(timeValue % 60)
+
+    outTitle.innerHTML = `The movie title is ${title}`
+    outTime.innerHTML = `${hours} hours and ${minutes} minutes`
+}
