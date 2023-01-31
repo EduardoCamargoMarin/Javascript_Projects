3a06b35b67b193307d60c8f7d0563d3feff9f0e1
Eduardo Marin <108038506+EduardoCamargoMarin@users.noreply.github.com>
2023-01-31 15:38:03 -0300

    Add files via upload

--- Revenda de veiculo/index.js
+++ Revenda de veiculo/index.js
@@ -1,9 +1,11 @@
 
 function calcular() {
     const vehicle = document.getElementById('inputVehicle').value
-    const year = Number(document.getElementById('inputYear'))
-    const price = Number(document.getElementById('inputPrice'))
+    const vehicleModel = document.getElementById('inputVehicleModel').value
+    const year = document.getElementById('inputYear').value
+    const price = document.getElementById('inputPrice').value
     const resultVehicle = document.getElementById('outputVehicle')
+    const resultVehicleModel = document.getElementById('outputVehicleModel')
     const resultYear =  document.getElementById('outputYear')
     const resultPrice = document.getElementById('outputPrice') 
     const resultPortion = document.getElementById('outputPortion')
@@ -14,12 +16,20 @@ function calcular() {
     //Valor parcelado
     const portionPayday = firstPayday / 12
 
+    
     resultVehicle.innerText = `Promoção: ${vehicle}`
+    resultVehicleModel.innerText = `Modelo: ${vehicleModel}`
     resultYear.innerText = `Ano: ${year}`
-    resultPrice.innerText = `Entrada: ${firstPayday}`
-    resultPortion.innerText = `${portionPayday}`
+    resultPrice.innerText = `Entrada: R$ ${firstPayday}`
+    resultPortion.innerText = `Parcelado: 12x de R$ ${portionPayday.toFixed(2)}`
 
-
-
-
+    if (price <=0 || year <= 0) {
+        alert('Valor invalido - Numeros negativos') 
+        resultVehicle.innerText = `Promoção:`
+        resultVehicleModel.innerText = `Modelo: `
+        resultYear.innerText = `Ano:`
+        resultPrice.innerText = `Entrada:`
+        resultPortion.innerText = `Parcelado: `
+    } 
+    
 }
