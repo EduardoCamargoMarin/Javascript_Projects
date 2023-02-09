54b0904941d24727cbbe0905ae5bf77981f3e32d
Eduardo Marin <108038506+EduardoCamargoMarin@users.noreply.github.com>
2023-02-09 17:47:58 -0300

    Add files via upload

---
+++ Impressão por valor/index.js
@@ -0,0 +1,21 @@
+
+// Jeito intuitivo
+//const namePrint = ['E','ED','EDU','EDUA','EDUAR','EDUARDD','EDAURDO']
+
+
+//for(let print of namePrint) {
+  //  console.log(print)
+//}
+
+//================================================================
+
+//Jeito correto
+
+const namePrint = ['E','D','U','A','R','D','O']
+
+for (let print of namePrint){
+    console.log(print)
+}
+
+
+
