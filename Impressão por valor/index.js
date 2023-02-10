e05f7d78358b1c383cab2621e84c5616163892da
Eduardo Marin <du.marin10@gmail.com>
2023-02-09 22:44:31 -0300

    js

--- Impressão por valor/index.js
+++ Impressão por valor/index.js
@@ -9,13 +9,35 @@
 
 //================================================================
 
+
+
+//jeito intuitivo 2
+//const namePrint = []
+
+//namePrint.push('E')
+//namePrint.push('ED')
+//namePrint.push('EDU')
+//namePrint.push('EDUA')
+//namePrint.push('EDUAR')
+//namePrint.push('EDUARD')
+//namePrint.push('EDUARDO')
+
+//for (let print of namePrint){
+  //  console.log(print)
+//}
+
+//========================================================
+
 //Jeito correto
 
 const namePrint = ['E','D','U','A','R','D','O']
 
-for (let print of namePrint){
-    console.log(print)
+for (const i of namePrint) {
+
+  console.log(i)
+  
 }
+
 
 
 
