55ddb1f01c4b76f638e2936761a4a7475ada7c99
Eduardo Marin <du.marin10@gmail.com>
2023-02-10 21:52:29 -0300

    js

---
+++ Taxa de entrega/index.js
@@ -0,0 +1,26 @@
+
+function calcular() {
+    //Entrada de dados
+    const plate01 = document.getElementById('plate01').checked
+    const plate02 = document.getElementById('plate02').checked
+    const plate03 = document.getElementById('plate03').checked
+    const plate04 = document.getElementById('plate04').checked
+
+    const payMoney = document.getElementById('payMoney').checked
+    const payCard = document.getElementById('payCard').checked
+
+    const sectorA = document.getElementById('sectorA').checked
+    const sectorB = document.getElementById('sectorB').checked
+    const sectorC = document.getElementById('sectorC').checked
+
+    const inputAddress = document.getElementById('inputAddress').checked
+//==========================================================================
+    //Saida de dados
+    const outputOrder = document.getElementById('outputOrder')
+    const outputPayment = document.getElementById('outputPayment')
+    const outputTip = document.getElementById('outputTip')
+    const outputTotal = document.getElementById('outputTotal')
+    const outputAddress = document.getElementById('outputAddress')
+
+    //falta adicionar o calculo e as condições
+}
