6c13afe035d3448cbde7a75e6d222d7d59c57141
Eduardo Marin <108038506+EduardoCamargoMarin@users.noreply.github.com>
2023-02-14 17:54:39 -0300

    Add files via upload

--- Taxa de entrega/index.js
+++ Taxa de entrega/index.js
@@ -1,4 +1,46 @@
 
+function addInfo() {
+    //Entrada de dados para trazer as informações do menu
+    const plateName01 = document.getElementById('inputPlate01').value
+    const plateName02 = document.getElementById('inputPlate02').value
+    const plateName03 = document.getElementById('inputPlate03').value
+    const plateName04 = document.getElementById('inputPlate04').value
+
+    const platePrice01 = document.getElementById('inputPrice01').value
+    const platePrice02 = document.getElementById('inputPrice02').value
+    const platePrice03 = document.getElementById('inputPrice03').value
+    const platePrice04 = document.getElementById('inputPrice04').value
+
+    const nameSectorA = document.getElementById('inputSectorA').value
+    const nameSectorB = document.getElementById('inputSectorB').value
+    const nameSectorC = document.getElementById('inputSectorC').value
+
+    //saida de dados
+    const outputPlateName01 = document.getElementById('outputPlate01')
+    const outputPlateName02 = document.getElementById('outputPlate02')
+    const outputPlateName03 = document.getElementById('outputPlate03')
+    const outputPlateName04 = document.getElementById('outputPlate04')
+
+    const outputPriceValue01 = document.getElementById('outputPlate01')
+    const outputPriceValue02 = document.getElementById('outputPlate02')
+    const outputPriceValue03 = document.getElementById('outputPlate03')
+    const outputPriceValue04 = document.getElementById('outputPlate04')
+
+    const outputSectorName01 = document.getElementById('outputSectorA')
+    const outputSectorName02 = document.getElementById('outputSectorB')
+    const outputSectorName03 = document.getElementById('outputSectorC')
+
+    
+
+}
+
+
+
+
+
+
+
+
 function calcular() {
     //Entrada de dados
     const plate01 = document.getElementById('plate01').checked
@@ -21,17 +63,6 @@ function calcular() {
     const outputTip = document.getElementById('outputTip')
     const outputTotal = document.getElementById('outputTotal')
     const outputAddress = document.getElementById('outputAddress')
-
-    //Determinar os valores de cada item
-    //Precisa ser vinculada com o valor que o usuário determine.
-    plateBill01 = 15
-    plateBill02 = 13
-    plateBill03 = 20
-    plateBill04 = 17
-
-    sectorATip = 3
-    sectorBTip = 5
-    sectorCTip = 7
 
     //Condições do programa
 
