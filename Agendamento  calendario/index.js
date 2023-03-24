e26684dfbbfd74791a77d9f2c9c9a435678460e2
Eduardo Marin <108038506+EduardoCamargoMarin@users.noreply.github.com>
2023-03-24 17:57:48 -0300

    Add files via upload

--- Agendamento  calendario/index.js
+++ Agendamento  calendario/index.js
@@ -4,35 +4,37 @@
 //======================================================
 //Criar o calendário.
 let calendar = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-let week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-let days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+let week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+let date = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 let time = [8,9,10,11,13,14,15,16,17]
 
 //constructor do cliente 01
 class appoitmentClient01 {
-    constructor (month, week, days, time) {
+    constructor (month, week, date, time) {
         this.month = month;
         this.week = week;
-        this.days = days;
+        this.date = date;
         this.time = time;
     }}
 
 //constructor do cliente 02
 class appoitmentClient02 {
-    constructor (month, week, days, time) {
+    constructor (month, week, date, time) {
         this.month = month;
         this.week = week;
-        this.days = days;
+        this.date = date;
         this.time = time;
     }}
 
-let clientAgenda01 = new appoitmentClient01()
+let clientAgenda01 = new appoitmentClient01(calendar[1], week[2], date[30], time[7])
 let clientAgenda02 = new appoitmentClient02()
-
-
-
-
+console.table(clientAgenda01)
 
 
 //validar se  as datas não condiz com 2 clientes (try catch)
 
+try {
+    let 
+} catch (error) { // Precisa finalizar!!
+    
+}
