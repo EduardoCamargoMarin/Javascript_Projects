df94fa244694c5c0d3d7971a6604475941d6011c
Eduardo Marin <108038506+EduardoCamargoMarin@users.noreply.github.com>
2023-03-28 17:48:35 -0300

    Add files via upload

---
+++ Question the AI/index.js
@@ -0,0 +1,24 @@
+// Criar um conceito do funcionamento de uma IA.
+//Todas as possíveis perguntas e comandos.
+const questions = [
+    'What is your name?',
+    'How old are you?',
+    'Where are you?',
+    'Are you close?'
+]
+
+//Todas as possíveis respostas para as perguntas ou comando
+const answer = [
+    'I am an AI in development',
+    '28 years old',
+    'Inside the network',
+    'away'
+]
+
+//Usuário realizando a ação de perguntar ou dar o comando.
+let questionAI = 'What is your name?'
+
+//AI respondendo.
+if(questionAI === questions[0]) {
+    console.log(answer[0])
+}
