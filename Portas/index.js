d08917fc880cf6950e23af3e168cb66b6bc1c929
Eduardo Marin <108038506+EduardoCamargoMarin@users.noreply.github.com>
2023-03-31 17:52:43 -0300

    Add files via upload

---
+++ Portas/index.js
@@ -0,0 +1,19 @@
+// Criar uma aplicação que funcione igual a regra de portas bloqueadas em jogos.
+let hashCodeNewApply = Math.ceil(Math.random()*8475634) 
+let infoHashUnlock = 'key_'
+
+let hashCodeUnlock = `${infoHashUnlock }${hashCodeNewApply}` // toda vez que iniciar uma aplicação vai gerar esse codigo
+
+let doorCloseMainHall = `${infoHashUnlock }${hashCodeNewApply}`
+console.log(`Main hall door ${doorCloseMainHall}`)
+console.log(`Main hall Item ${hashCodeUnlock}`)
+
+if(hashCodeUnlock === doorCloseMainHall) {
+    console.log("Door unlock")
+}
+else{
+    console.error(`This key does not belong here`)
+}
+
+//A regra de itens que abrem portas nos jogos se baseiam em itens que possuem o mesmo codigo para a confirmação.
+
