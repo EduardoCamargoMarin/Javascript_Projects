9979e07b71f34abf4ec9532074cd3698e6a4c586
Eduardo Marin <108038506+EduardoCamargoMarin@users.noreply.github.com>
2023-04-06 17:58:59 -0300

    Add files via upload

--- Regra de acesso/index.js
+++ Regra de acesso/index.js
@@ -2,22 +2,41 @@
 
 //Criação de um armazenamento de dados para os projetos que desejam ter acesso ao API.
 class cityPermission{
-    constructor(city, projectCode, id_permission, ip_external_access) {
+    constructor(city, ip_external_access) {
         this.city = city;
-        this.projectCode = projectCode;
-        this.id_permission = id_permission;
         this.ip_external_access = ip_external_access;
     }
 }
 
-//teste de acesso a API;
-let externalAccess = []
-
-//regra para o acesso liberar IP permitido e bloquear os que estão  fora da regra.
-
-while(externalAccess in cityPermission) {
-    console.log('Access granted')
-}
 
 //Entrada do dado do IP publico na regra
-let external_IP = new cityPermission('BRAGANÇA PAULISTA', 254159, )
+ 
+    const BRAGANCAPAULISTA = new cityPermission('BRAGANCA PAULISTA', '187.58.22.49')
+    const ITATIBA = new cityPermission('ITATIBA', '147.48.66.154')
+    const MORUNGABA = new cityPermission('MORUNGABA', '155.64.33.222')
+    const CAMPINAS = new cityPermission('CAMPINAS', '145.65.31.201') // Única cidade não autorizada
+    const JUNDIAI = new cityPermission('JUNDIAI', '166.44.23.112')
+
+
+//Teste de conexão com a API.
+let testAccess = [CAMPINAS]
+
+//Adicionar os IPs na regra de acesso
+let cityAccess = [BRAGANCAPAULISTA, ITATIBA, MORUNGABA, JUNDIAI]
+
+//Condição de acesso
+if(cityAccess.includes(testAccess[0])) {
+    console.log('Access granted')
+}
+else {
+    console.error('Invalid Access')
+}
+
+
+
+
+
+
+
+
+ 
