add31d8630337dce0f0f74cacbe0578cfdbe9179
Eduardo Marin <108038506+EduardoCamargoMarin@users.noreply.github.com>
2023-04-18 18:01:26 -0300

    Add files via upload

--- Gerencia de estoque/versão 01/terminal.js
+++ Gerencia de estoque/versão 01/terminal.js
@@ -2,16 +2,25 @@
 //Armazenamento de dados dos nomes dos produtos
 // Setor A é destinado aos produtos de classificação "ELETRONICO"
 class sectorA {
-  constructor(product_name,ID, ) {
+  constructor(product_name,ID, incomeQt,updateDate, outcomeQt, totalQt ) {
         this.product = product_name;
         this.ID = ID;
+        this.date = updateDate;
+        this.income_Quantity = incomeQt;
+        this.outcome_Quantity = outcomeQt;
+        this.total_Quantity = totalQt;
     }
 }
 // Setor B é destinado aos produtos de classificação "ADMINISTRATIVO"
 class sectorB {
-    constructor(product_name,ID, ) {
+    constructor(product_name,ID, incomeQt,updateDate, outcomeQt, totalQt ) {
         this.product = product_name;
         this.ID = ID;
+        this.date = updateDate;
+        this.income_Quantity = incomeQt;
+        this.outcome_Quantity = outcomeQt;
+        this.total_Quantity = totalQt;
+
     }
 }
 
@@ -24,27 +33,25 @@ class sectorB {
 //             >> Itens do setor A <<
 //====================================================
 
-const A_001 = new sectorA ('MAQUINA CARTAO CLASSY', '02234-90')
-const A_002 = new sectorA ('IMPRESSORA EPSON JATO', '03322-01')
-const A_003 = new sectorA ('CAMERA VIDEO SERVER IP', '11103-45')
+
+
+const A_001 = new sectorA ('MAQUINA CARTAO CLASSY', '02234-90', 30, '15/04/2023', 0,)
+const A_002 = new sectorA ('IMPRESSORA EPSON JATO', '03322-01',22,'10/04/2023',0)
+const A_003 = new sectorA ('CAMERA VIDEO SERVER IP', '11103-45', 30, '15/04/2023', 0)
 
 let sectorA_products = [A_001, A_002, A_003] // para visualização
-
-console.table(sectorA_products)
 
 //====================================================
 //             >> Itens do setor B <<
 //====================================================
 
-const B_001 = new sectorB ('BLOCO DE NOTAS 5X5', '12534-94')
-const B_002 = new sectorB ('CAIXA CANETA BIC AZUL 100X', '59862-01')
-const B_003 = new sectorB ('GRAMPEADOR XEIN BLACK', '32010-05')
-const B_004 = new sectorB ('GRAMPO XINGU 250X', '32690-05')
+const B_001 = new sectorB ('BLOCO DE NOTAS 5X5', '12534-94',22,'10/04/2023',0)
+const B_002 = new sectorB ('CAIXA CANETA BIC AZUL 100X', '59862-01', 30, '15/04/2023', 0)
+const B_003 = new sectorB ('GRAMPEADOR XEIN BLACK', '32010-05',22,'10/04/2023',0)
+const B_004 = new sectorB ('GRAMPO XINGU 250X', '32690-05', 30, '15/04/2023', 0)
 
 let sectorB_products = [B_001, B_002, B_003, B_004] // para visualização
 
 console.table(sectorA_products.concat(sectorB_products)) // concat se quiser juntar todas
 
-
-
-
+//==========================================================================================
